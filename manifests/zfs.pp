f2acef01d79607de87719712d9d4e5d50500745a
Jakov Sosic <jsosic@gmail.com>
2016-09-26 12:09:54 +0200

    Add support for ZFS arc cache config

--- manifests/zfs.pp
+++ manifests/zfs.pp
@@ -3,9 +3,19 @@
 #
 # This class installs ZFSonLinux
 #
-class tools::zfs {
+class tools::zfs (
+  $zfs_arc_max = '2147483648',
+){
 
   include ::yum::repo::zfs
   package { 'zfs': ensure => present, }
+
+  file { '/etc/modprobe.d/zfs.conf':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('tools/zfs/zfs.erb')
+  }
 
 }
