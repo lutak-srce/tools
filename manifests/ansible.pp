d756138624b77a3a2b69ca5c54548b785870334e
Jakov Sosic <jsosic@gmail.com>
2018-08-09 13:26:20 +0200

    Set boto3 and botocore as ansible deps

--- manifests/ansible.pp
+++ manifests/ansible.pp
@@ -5,6 +5,8 @@ class tools::ansible (
 ) {
   include ::yum::repo::epel
   include ::python::mod::boto
+  include ::python::mod::boto3
+  include ::python::mod::botocore
 
   package { 'ansible' :
     ensure => present,
